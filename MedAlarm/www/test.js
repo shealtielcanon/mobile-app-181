8e8074496ac790dfa27466588e483c1a76230f54
FrancoJigo <frankeejigzz@gmail.com>
2017-12-16 07:52:44 +0800

    added dashboard

--- MedAlarm/www/test.js
+++ MedAlarm/www/test.js
@@ -1,3 +1,16 @@
-$("#click").click(function(){
-  $("#nav").toggleClass("closed");
+$(document).ready(function(){
+$("#mytable #checkall").click(function () {
+        if ($("#mytable #checkall").is(':checked')) {
+            $("#mytable input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#mytable input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+    
+    $("[data-toggle=tooltip]").tooltip();
 });
