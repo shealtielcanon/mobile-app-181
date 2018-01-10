051cb5ed56d6fbd3979d0788d206baf641d13df6
mieken1914 <31238385+mieken1914@users.noreply.github.com>
2018-01-11 06:35:03 +0800

    Minor

--- MedAlarm/www/js/database_access.js
+++ MedAlarm/www/js/database_access.js
@@ -773,7 +773,7 @@ function newLogEntry(ev_id, e_name, e_desc) { //check and debug this!
 
 function updateLogText(ev_id, added_text) {
     db.transaction(function(transaction) {
-        var executeQuery = "UPDATE logs SET generated_text = generated_text || ', +"added_text"+' WHERE e_id=?";
+        var executeQuery = "UPDATE logs SET generated_text = generated_text || '," +added_text+"' WHERE e_id=?";
         transaction.executeSql(executeQuery, [ev_id],nullHandler,errorHandler);
     },
     function (error) {
